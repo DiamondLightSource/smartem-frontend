a635abe70a0dce11a685347c6b1de5a8b2e4e98b
Dan Hatton <daniel.hatton@diamond.ac.uk>
2025-04-27 17:44:18 +0100

    Order grid square info table based on number of holes and hole table based on score

--- app/routes/grid.tsx
+++ app/routes/grid.tsx
@@ -1,6 +1,6 @@
 import type { Route } from "./+types/product";
 
-import { Box, Container, Collapse, TableContainer, IconButton, Table, TableHead, TableRow, TableCell, TableBody, ThemeProvider, CircularProgress } from "@mui/material";
+import { Box, Container, Collapse, TableContainer, IconButton, Table, TableHead, TableRow, TableCell, TableBody, ThemeProvider, CircularProgress, TableSortLabel } from "@mui/material";
 import Paper from '@mui/material/Paper';
 import InsightsIcon from "@mui/icons-material/Insights";
 
@@ -37,16 +37,17 @@ export async function loader({ params }: Route.LoaderArgs) {
     .then((response) => {return response.json()})
     .then((holes) => {return { square: square, holes: holes }}))
   );
-  const weightedPredictions: Promise<HolePrediction[][]> = Promise.all(
+  const weightedPredictions = Promise.all(
     squares_data.map((square) => fetch(`http://localhost:8000/gridsquares/${square.id}/weighted_predictions`)
     .then((response) => {return response.json()})
-    .then((scores) => { return Object.entries(scores as {[key: number]: Score[]}).map(([fh, elem]) => {return { prediction: elem.slice(-1)[0].value, hole: fh } }) }))
+    .then((scores) => { return [square.name, Object.entries(scores as {[key: number]: Score[]}).map(([fh, elem]) => {return { prediction: elem.slice(-1)[0].value, hole: fh } })] }))
   );
   return { squares, weightedPredictions };
 }
 
 const CollapsibleRow = ({ square, holes, weightedPredictions }: FullSquareDetails) => {
     const [open, setOpen] = React.useState(false);
+    const [sortOrderDescending, setSortOrderDescending] = React.useState(true);
     const navigate = useNavigate();
 
     let weightedPrediction = null;
@@ -54,6 +55,19 @@ const CollapsibleRow = ({ square, holes, weightedPredictions }: FullSquareDetail
     if(weightedPredictions) {
       weightedPrediction = (weightedPredictions).reduce((a: number, b: HolePrediction) => a + b.prediction, 0) / weightedPredictions.length;
       weightedPredictions.map((elem) => holeWeights.set(elem.hole, elem.prediction));
+    }
+
+    const holeWeightComparator = (a: FoilHoleResponse, b: FoilHoleResponse) => {
+      if(sortOrderDescending) {
+        if(holeWeights.get(a.name) < holeWeights.get(b.name)) return 1;
+        else if(holeWeights.get(a.name) > holeWeights.get(b.name)) return -1;
+        return 0;
+      }
+      else {
+        if(holeWeights.get(a.name) < holeWeights.get(b.name)) return -1;
+        else if(holeWeights.get(a.name) > holeWeights.get(b.name)) return 1;
+        return 0;
+      }
     }
 
     return (
@@ -86,11 +100,15 @@ const CollapsibleRow = ({ square, holes, weightedPredictions }: FullSquareDetail
                                 <TableHead>
                                     <TableRow>
                                         <TableCell style={{ backgroundColor: "silver" }}>Hole Name</TableCell>
-                                        <TableCell style={{ backgroundColor: "silver" }}>Score</TableCell>
+                                        <TableCell style={{ backgroundColor: "silver" }}>
+                                          <TableSortLabel active={true} direction={sortOrderDescending ? "desc": "asc"} onClick={() => {setSortOrderDescending(!sortOrderDescending)}}>
+                                            Score
+                                          </TableSortLabel>
+                                        </TableCell>
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
-                                    {holes.map((hole) => {return <TableRow key={hole.id}>
+                                    {holes.sort(holeWeightComparator).map((hole) => {return <TableRow key={hole.id}>
                                       <TableCell>{hole.name}</TableCell>
                                       {(weightedPrediction === null) ?
                                       <TableCell><CircularProgress size={10}/></TableCell>:
@@ -108,6 +126,22 @@ const CollapsibleRow = ({ square, holes, weightedPredictions }: FullSquareDetail
 }
 
 export default function Grid({ loaderData }: Route.ComponentProps) {
+  const [holeNumberOrder, setHoleNumberOrder] = React.useState(true);
+  const [sortOrderDescending, setSortOrderDescending] = React.useState(true);
+
+  const holeNumberComparator = (a: SquareDetails, b: SquareDetails) => {
+    if(sortOrderDescending) {
+      if(a.holes.length < b.holes.length) return 1;
+      else if(a.holes.length > b.holes.length) return -1;
+      return 0;
+    }
+    else {
+      if(a.holes.length < b.holes.length) return -1;
+      else if(a.holes.length > b.holes.length) return 1;
+      return 0;
+    }
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Navbar/>
@@ -119,7 +153,11 @@ export default function Grid({ loaderData }: Route.ComponentProps) {
                 <TableCell>Grid Square ID</TableCell>
                 <TableCell>Grid Square Name</TableCell>
                 <TableCell>Status</TableCell>
-                <TableCell>Number of Holes</TableCell>
+                <TableCell sortDirection={holeNumberOrder ? sortOrderDescending ? "desc": "asc": false}>
+                  <TableSortLabel active={holeNumberOrder} direction={sortOrderDescending ? "desc": "asc"} onClick={() => {setSortOrderDescending(!sortOrderDescending)}}>
+                    Number of Holes
+                  </TableSortLabel>
+                </TableCell>
                 <TableCell>Score</TableCell>
                 <TableCell>More Info</TableCell>
               </TableRow>
@@ -134,12 +172,15 @@ export default function Grid({ loaderData }: Route.ComponentProps) {
             }>
             <Await resolve={loaderData.weightedPredictions}>
               { (result) =>
-              <TableBody>
-                {loaderData.squares.map((square: SquareDetails, i: number) => {
-                  return <CollapsibleRow square={square.square} holes={square.holes} weightedPredictions={result[i]} />
+              {
+              const mapResult: Map<string, HolePrediction[]> = new Map(result);
+              return <TableBody>
+                {loaderData.squares.sort(holeNumberComparator).map((square: SquareDetails) => {
+                  return <CollapsibleRow square={square.square} holes={square.holes} weightedPredictions={mapResult.get(square.square.name) ?? []} />
                 })
                 }
               </TableBody>
+              }
 }
             </Await>
           </React.Suspense>
