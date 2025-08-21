4f6ea54b0468039a9e051b25760f0f62f9c01f48
Dan Hatton <daniel.hatton@diamond.ac.uk>
2025-08-21 15:04:41 +0100

    fixing up some of the page that displays quality predictions for grid squares

--- app/routes/qualityPredictions.tsx
+++ app/routes/qualityPredictions.tsx
@@ -1,6 +1,6 @@
 import type { Route } from "./+types/product";
 
-import { Box, Grid, ThemeProvider, Card, CardContent, Container, IconButton } from "@mui/material";
+import { Box, Grid, ThemeProvider, Card, CardContent, Container, IconButton, Slider, Stack } from "@mui/material";
 import { BarChart as BarChartIcon, Science, Timeline } from "@mui/icons-material";
 import { BarChart, LineChart } from '@mui/x-charts';
 
@@ -20,70 +20,67 @@ import type { components } from "../schema"
 import { apiUrl } from "../utils/api";
 
 type QualityPrediction = components["schemas"]["QualityPrediction"]
-type Score = components["schemas"]["Score"]
-type PredictionResponse = { [key: string]: QualityPrediction[][] }
+type QualityPredictionModelWeight = components["schemas"]["QualityPredictionModelWeight"]
 
 export async function loader({ params }: Route.LoaderArgs) {
-  const grids = await fetch(`${apiUrl()}/gridsquares/${params.squareId}/quality_predictions`);
-  const weightedPredictions: Promise<Score[][]> = fetch(`${apiUrl()}/gridsquares/${params.squareId}/weighted_predictions`)
-    .then((response) => {return response.json()})
-    .then((scores) => { return Object.entries(scores as {[key: number]: Score[]}).map(([fh, elem]) => {return elem}) })
-    .then(m => {return m[0].map((x,i) => {return m.map(x => x[i])})})
-  ;
-  //const weightedPredictions = weightedPredictionsAll.map((elem) => {return elem.reduce((a, b) => a + b.value, 0) / elem.length});
-  //const predictionTimes = weightedPredictionsAll.map((elem) => {return Date.parse(elem[0].timestamp)});
-  const result  = await grids.json();
-  return { result, weightedPredictions };
+  const squarePredictionsResponse = await fetch(`${apiUrl()}/gridsquares/${params.squareId}/quality_predictions`);
+  const holePredictionsResponse = await fetch(`${apiUrl()}/gridsquares/${params.squareId}/foilhole_quality_predictions`);
+  const modelWeights = await fetch(`${apiUrl()}/grids/${params.gridId}`)
+  const squarePredictions  = await squarePredictionsResponse.json();
+  const holePredictions  = await holePredictionsResponse.json();
+  return { squarePredictions, holePredictions, modelWeights };
 }
 
-const PredictionCharts = ({ predictions }: { predictions: QualityPrediction[][] }) => {
-  const [showTimeSeries, setShowTimeSeries] = React.useState(false);
-  const histGenerator = bin().domain([0, 1]).thresholds(19);
 
-  const ChartSwitcher = () => {
-    return <IconButton onClick={() => {setShowTimeSeries(!showTimeSeries)}} sx={{ marginBottom: "auto" }}>
-      {showTimeSeries ? <BarChartIcon/>: <Timeline/>}
-    </IconButton>
-  }
-
-  return (predictions.length > 1 ? 
-    showTimeSeries ?
-    <Box sx={{ display: "flex", alignItems: "center", margin: "5px" }}>
+const GridSquarePredictionCharts = ({ predictions }: { predictions: QualityPrediction[] }) => {
+  return (<Box sx={{ display: "flex", alignItems: "center", margin: "5px" }}>
       <LineChart
           hideLegend
           grid={{ horizontal: true }}
-          xAxis={predictions.map((elem) => {
-            return {
-                id: `timestamp-${elem.slice(-1)[0].foilhole_id}`,
-                data: elem.map((e) => {return e.timestamp? Date.parse(e.timestamp): 0}),
-                scaleType: "time",
-            }})
-          }
-          series={predictions.map((elem) => {
-            return {
-                id: `prediction-${elem.slice(-1)[0].foilhole_id}`,
-                data: elem.map((e) => {return e.value}),
-                label: elem.slice(-1)[0].foilhole_id?.toString()
-            }})
-          }
-          yAxis={[{ min:0, max: 1 }]}
+          xAxis={[{data: predictions.map((elem) => {return elem.timestamp? Date.parse(elem.timestamp): 0}), scaleType: "time"}]} 
+          series={[{data: predictions.map((elem) => {return elem.value})}]}
+          yAxis={[{ min: 0, max: 1 }]}
           height={400}
       />
-      <ChartSwitcher/>
-    </Box>:
+    </Box>
+  )
+}
+
+const FoilHolePredictionCharts = ({ predictions }: { predictions: Map<string, QualityPrediction[]> }) => {
+  const mostRecentTimestamp = Math.max(...Object.entries(predictions).map(([key, value]) => {return Date.parse(value.slice(-1)[0].timestamp)}));
+  const leastRecentTimestamp = Math.min(...Object.entries(predictions).map(([key, value]) => {return Date.parse(value[0].timestamp)}));
+  const [selectedTime, setSelectedTime] = React.useState(mostRecentTimestamp);
+  const histGenerator = bin().domain([0, 1]).thresholds(19);
+
+  const getMostRecentBefore = (preds: QualityPrediction[]) => {
+    let current = preds[0];
+    for (const pred of preds) {
+      if (Date.parse(pred.timestamp) > selectedTime) {
+        return current;
+      }
+      else {
+        current = pred;
+      }
+    }
+    return current;
+  }
+
+  return ((!!Object.entries(predictions).length) ? 
     <Box sx={{ display: "flex", alignItems: "center", margin: "5px" }}>
+    <Stack spacing={1}>
       <BarChart 
         xAxis={[{ scaleType: "band", 
                   data: histGenerator(
-                    predictions.map((elem) => {
-                      return elem.slice(-1)[0].value})).map((b) => {return (b.x0 !== undefined && b.x1 !== undefined) ? 
-                        b.x0 + (b.x1-b.x0): 0}) 
+                    Object.entries(predictions).map(([key, value]) => {
+                      return value.slice(-1)[0].value
+                    })).map((b) => {return (b.x0 !== undefined && b.x1 !== undefined) ? b.x0 + (b.x1-b.x0): 0})
                 }]} 
         series={[{ 
-          data: histGenerator(predictions.map((elem) => {return elem.slice(-1)[0].value})).map((b) => {return b.length}),
+          data: histGenerator(Object.entries(predictions).map(([key, value]) => {return getMostRecentBefore(value).value})).map((b) => {return b.length}),
           color: "#5C9EAD" 
         }]} />
-      <ChartSwitcher/>
+        <Slider min={leastRecentTimestamp} max={mostRecentTimestamp} value={selectedTime} onChange={(event: Event, newValue: number[]) => setSelectedTime(newValue)} />
+    </Stack>
     </Box>: <></>
   )
 }
@@ -92,44 +89,43 @@ const PredictionCharts = ({ predictions }: { predictions: QualityPrediction[][] 
 export default function QualityPredictionsForSquare({ loaderData, params }: Route.ComponentProps) {
   const navigate = useNavigate();
 
-  const mostRecentsMean = (arr: QualityPrediction[][]) => {
-    const mostRecents = arr.map((el) => {return el.slice(-1)[0].value});
+  const mostRecentsMean = (fhPredictions: Map<string, QualityPrediction[]>) => {
+    const mostRecents = Object.entries(fhPredictions).map(([key, el]) => {return el.slice(-1)[0].value});
     return mostRecents.reduce((a: number, b: number) => a + b, 0) / mostRecents.length;
   }
 
   return <ThemeProvider theme={theme}>
     <Navbar/>
     <Container content="center" style={{ width: "100%", paddingTop: "50px" }}> 
-      <React.Suspense fallback={
-        <TimeSeriesChart xData={[]} yData={[]} />
-      }
-      >
-        <Await resolve={loaderData.weightedPredictions}>
-          {(result: Score[][]) => 
-            <TimeSeriesChart 
-              xData={result.map((elem) => {return Date.parse(elem[0].timestamp)})} 
-              yData={result.map((elem) => {return elem.reduce((a, b) => a + b.value, 0) / elem.length})}
-            />
-          }
-        </Await>
-      </React.Suspense>
       <Grid container spacing={3} sx={{ padding: "20px" }}>
         {      
-          Object.entries(loaderData.result as PredictionResponse).map(([key, value]) => 
+          Object.entries(loaderData.squarePredictions).map(([key, value]) => 
             {return (
               <Grid size={4}>
                 <Card>
-                  <Box sx={{ display: "flex", alignItems: "center", margin: "5px" }}>                
-                    <CardContent>Model: {key}</CardContent>
-                    <IconButton onClick={() => {navigate(`/models/${key}/grids/${params.gridId}/weights`)}} style={{ marginLeft: "auto" }}>
-                      <Science/>
-                    </IconButton>  
-                  </Box>
-                  <CardContent sx={{ margin: "5px" }}>
-                    {mostRecentsMean(value).toLocaleString(undefined, { maximumSignificantDigits: 3, minimumSignificantDigits: 3 })}
+                  <CardContent style={{ backgroundColor: "#b927d9" }}>Model: {key}</CardContent>
+                  <CardContent style={{ backgroundColor: "#b927d9" }}>
+                    {value.slice(-1)[0].value.toLocaleString(undefined, { maximumSignificantDigits: 3, minimumSignificantDigits: 3 })}
                   </CardContent>
-                  <CardContent>
-                    <PredictionCharts predictions={value} />
+                  <CardContent style={{ backgroundColor: "#b927d9" }}>
+                    <GridSquarePredictionCharts predictions={value} />
+                  </CardContent>
+                </Card>
+              </Grid>
+            )
+          })
+        }
+        {      
+          Object.entries(loaderData.holePredictions).map(([key, value]) => 
+            {return (
+              <Grid size={4}>
+                <Card>
+                  <CardContent style={{ backgroundColor: "#b927d9" }}>Model: {key}</CardContent>
+                  <CardContent style={{ backgroundColor: "#b927d9" }}>
+                  {mostRecentsMean(value as Map<string, QualityPrediction[]>).toLocaleString(undefined, { maximumSignificantDigits: 3, minimumSignificantDigits: 3 })}
+                  </CardContent>
+                  <CardContent style={{ backgroundColor: "#b927d9" }}>
+                    <FoilHolePredictionCharts predictions={value} />
                   </CardContent>
                 </Card>
               </Grid>
